1f46316292d2fd79d3241561fe8e2295a9015350
Muhammad Nosherwan <muhammadnosherwan51@gmail.com>
2025-12-11 18:15:39 +0500

    Checking-Duplicates in arrays

--- Arrays/checking-duplicates.cpp
+++ Arrays/checking-duplicates.cpp
@@ -1,31 +1,26 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+// This program takes array and checks if any number in the array exists twice
+// or not.
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
-vector<int> findDuplicates(vector<int>& arr)
-{
-    vector<int> arr1;
-    sort(arr.begin(),arr.end());
-    for(int i=1;i<arr.size();i++)
-        {
-            if(arr[i]==arr[i-1])
-            {
-                if(arr1.empty() || arr1.back()!=arr[i])
-                {
-                    arr1.push_back(arr[i]);
-                }
-                
-            }
-        }
-    return arr1;
+vector<int> findDuplicates(vector<int> &arr) {
+  vector<int> arr1;
+  sort(arr.begin(), arr.end());
+  for (int i = 1; i < arr.size(); i++) {
+    if (arr[i] == arr[i - 1]) {
+      if (arr1.empty() || arr1.back() != arr[i]) {
+        arr1.push_back(arr[i]);
+      }
+    }
+  }
+  return arr1;
 }
-int main()
-{
-	vector<int> vect={1,2,3,5,24,6,33,66,3,5,2,6,3,6,5,44,6};
-	vector<int> solution=findDuplicates(vect);
-	for(int i:solution)
-	{
-		cout<<i<<" ";
-	}
+int main() {
+  vector<int> vect = {1, 2, 3, 5, 24, 6, 33, 66, 3, 5, 2, 6, 3, 6, 5, 44, 6};
+  vector<int> solution = findDuplicates(vect);
+  for (int i : solution) {
+    cout << i << " ";
+  }
+  return 0;
 }
-
